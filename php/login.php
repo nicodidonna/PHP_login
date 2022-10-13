6283f922f78227af2ee8af55da2c9d5fe0a8a648
Nico Didonna <nico_didonna@yahoo.it>
2022-10-13 09:43:12 +0200

    login completato

---
+++ php/login.php
@@ -0,0 +1,38 @@
+<?php 
+
+require_once('config.php');
+
+$username = $connessione->real_escape_string($_POST['username']);
+$password = $connessione->real_escape_string($_POST['password']);
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+
+    $sql_select = "SELECT * FROM utenti WHERE username = '$username'";
+    if($result = $connessione->query($sql_select)){
+
+        if($result->num_rows == 1){
+            $row = $result->fetch_array(MYSQLI_ASSOC);
+            if(password_verify($password, $row['password'])){
+                session_start();
+
+                $_SESSION['loggato'] = true;
+                $_SESSION['id'] = $row['id_utente'];
+                $_SESSION['username'] = $row['username'];
+
+                header("location: ./area-privata.php");
+            }else{
+                echo "La password non è corretta";
+            }
+        }else{
+            echo "Non ci sono account con quello username";
+        }
+
+    }
+
+}else{
+    echo "Errore in fase di login";
+}
+
+$connessione->close();
+
+?>
--- php/register.php
+++ php/register.php
@@ -5,8 +5,9 @@ require_once('config.php');
 $email = $connessione->real_escape_string($_POST['email']);
 $username = $connessione->real_escape_string($_POST['username']);
 $password = $connessione->real_escape_string($_POST['password']);
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-$sql =" INSERT INTO utenti (email, username, password) VALUES ('$email','$username','$password')";
+$sql =" INSERT INTO utenti (email, username, password) VALUES ('$email','$username','$hashed_password')";
 
 if($connessione->query($sql)==true){
     echo "Registrazione effettuata con successo";
